6f9fd9a91b2d7e30b83c3bba6bd92c8376c7acc3
gurleynm <gurelynm@miamioh.edu>
2019-11-02 09:45:56 -0400

    "Howdy" test initiated. Intend to delete later.

--- app/src/main/java/com/android/arc/MainActivity.java
+++ app/src/main/java/com/android/arc/MainActivity.java
@@ -2,6 +2,7 @@ package com.android.arc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tx = findViewById(R.id.howdy);
+        tx.setText("Howdy planet");
     }
 }
